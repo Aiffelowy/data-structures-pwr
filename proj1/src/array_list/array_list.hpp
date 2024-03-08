59f0e072e5518c7a21e21ffb7d2b1731e92d1243
Aiffelowy <adas.krzywicki2@gmail.com>
2024-03-08 14:02:59 +0100

    implemented copy constructor/assignment and fixed the array constructor

--- proj1/src/array_list/array_list.hpp
+++ proj1/src/array_list/array_list.hpp
@@ -2,28 +2,29 @@
 #define MY_ARRAY_LIST
 
 
-#include <utility>
+#include <algorithm>
+
 template <typename T>
 struct ArrayList {
 private:
-  T* array;
+  T* buffer;
   int length;
-  int size;
+  int capacity;
 
 public:
   //default constructor
   ArrayList(void);
   //default constructor with optional preallocated size 
   ArrayList(int);
-  //array like constructor -- im sooo sorry
-  template<typename ... Ts>
-  ArrayList(Ts && ... values) : array(new T[sizeof...(Ts)]{ std::move(values) ... }), length(sizeof...(Ts)), size(sizeof...(Ts)) {}
   //destructor
   ~ArrayList();
   //copy constructor
   ArrayList(const ArrayList&);
   //copy assignment
   ArrayList& operator=(const ArrayList&);
+  //array like constructor -- im sooo sorry
+  template<typename ... Ts>  requires((... && std::is_convertible_v<Ts, T>))
+  ArrayList(Ts && ... values);
   //array accessor
   T& operator[](int);
  
@@ -31,6 +32,10 @@ public:
   T* begin(void);
   //iterator end
   T* end(void);
+  //const iterator begin
+  T* cbegin(void) const;
+  //const iterator end
+  T* cend(void) const;
 
   //push the new item at the end of the array
   void push(T);
@@ -44,33 +49,58 @@ public:
   void extend(int = 0);
   //resizes the array to provided size - items that dont fit the new size are lost
   void resize(int);
-  
-
-  //tests
-  int test_s() { return size; }
-  int test_l() { return length; }
+  //returns the size of the array
+  int size(void) const;
+  //return the length of the array
+  int len(void) const;
 };
 
 template<typename T>
-ArrayList<T>::ArrayList(void) : array(nullptr), length(0), size(0) {}
+ArrayList<T>::ArrayList(void) : buffer(nullptr), length(0), capacity(0) {}
 
 template<typename T>
-ArrayList<T>::ArrayList(int size) : array(new T[size]), length(0), size(size) {}
+ArrayList<T>::ArrayList(int size) : buffer(new T[size]), length(0), capacity(size) {}
 
 template<typename T>
-ArrayList<T>::~ArrayList(void) { delete [] array; }
+template<typename... Ts> requires((... && std::is_convertible_v<Ts, T>))
+ArrayList<T>::ArrayList(Ts&& ... values) : buffer(new T[sizeof...(Ts)]{ std::move(values) ... }), length(sizeof...(Ts)), capacity(sizeof...(Ts)) {}
 
 template<typename T>
-ArrayList<T>::ArrayList(const ArrayList& other) {}
+ArrayList<T>::~ArrayList(void) { delete [] buffer; }
 
 template<typename T>
-ArrayList<T>& ArrayList<T>::operator=(const ArrayList& other) {}
+ArrayList<T>::ArrayList(const ArrayList& other) {
+  this->length = other.length;
+  this->capacity = other.capacity;
+  if(this->capacity == 0) { this->buffer = nullptr; return; }
+
+  this->buffer = new T[this->capacity];
+  std::copy(other.cbegin(), other.cend(), this->begin());
+}
 
 template<typename T>
-T& ArrayList<T>::operator[](int i) {}
+ArrayList<T>& ArrayList<T>::operator=(const ArrayList& other) {
+  if(this == &other) { return *this; }
+
+  this->length = other.length;
+  this->size = other.capacity;
+  if(this->buffer != nullptr) {
+    delete [] this->buffer;
+  }
+  if(this->capacity == 0) { this->buffer = nullptr; return *this; }
+  this->buffer = new T[this->capacity];
+  std::copy(other.cbegin(), other.cend(), this->begin());
+  return *this;
+}
 
 template<typename T>
-void ArrayList<T>::push(T item) {}
+T& ArrayList<T>::operator[](int i) {
+  return this->buffer[i];
+}
+
+template<typename T>
+void ArrayList<T>::push(T item) {
+}
 
 template<typename T>
 void ArrayList<T>::insert(T item, int position) {}
@@ -88,10 +118,21 @@ template<typename T>
 void ArrayList<T>::resize(int new_size) {}
 
 template<typename T>
-T* ArrayList<T>::begin(void) { return &array[0]; }
+T* ArrayList<T>::begin(void) { return &buffer[0]; }
 
 template<typename T>
-T* ArrayList<T>::end(void) { return &array[length]; }
+T* ArrayList<T>::end(void) { return &buffer[length]; }
 
+template<typename T>
+T* ArrayList<T>::cbegin(void) const { return &buffer[0]; }
+
+template<typename T>
+T* ArrayList<T>::cend(void) const { return &buffer[length]; }
+
+template<typename T>
+int ArrayList<T>::size() const { return capacity; }
+
+template<typename T>
+int ArrayList<T>::len() const { return length; }
 
 #endif
--- proj1/src/main.cpp
+++ proj1/src/main.cpp
@@ -1,12 +1,22 @@
 #include "array_list/array_list.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  ArrayList<int> array = {1 ,2 ,3 ,4, 5, 6};
-  std::cout << array.test_s() << " " << array.test_l() << std::endl;
-  for(int a : array) {
-    std::cout << a << " ";
+template<typename T>
+void print_array_copy(ArrayList<T> arr) {
+  std::cout << "len: " << arr.len() << " size: " << arr.size() << std::endl;
+  for(T item : arr) {
+    std::cout << item << " ";
   }
   std::cout << std::endl;
+}
+
+
+int main(int argc, char *argv[]) {
+  ArrayList<int> arr1 = {1 ,2 ,3 ,4, 5, 6};
+  ArrayList<std::string> arr2 = {"abc", "def", "sus"};
+  
+  print_array_copy(arr1);
+  print_array_copy(arr2);
+  
   return 0;
 }
