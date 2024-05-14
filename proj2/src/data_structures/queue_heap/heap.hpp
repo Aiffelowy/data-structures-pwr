ea796c2829d92fd7d06d7e5bf525a5b7e60e2170
Aiffelowy <adas.krzywicki2@gmail.com>
2024-05-14 19:11:01 +0000

    added comments to pq heap

--- proj2/src/data_structures/queue_heap/heap.hpp
+++ proj2/src/data_structures/queue_heap/heap.hpp
@@ -12,44 +12,53 @@ private:
   int length = 0;
   T* buffer = nullptr;
 
+  //function that inserts an item at the end of internal buffer
   void insert_to_buffer(T);
+  //fuction to remove the last item in internal buffer
   void remove_from_buffer();
 
+  //resized the internal buffer to the desired size
   void resize(int);
+  //doubles the size of the internal buffer
   void expand_buffer();
+  //expands the internal buffer by x spaces
   void expand_buffer(int);
 
+  //returns the index of the parent for the given index of an item
   constexpr int parent(int i);
+  //returns the index of the left child of an item
   constexpr int left_child(int i);
+  //returns the index of the right child of an item
   constexpr int right_child(int i);
 
 public:
+  //default constructor
   Heap();
-
+  //constructor that preallocates some space
   Heap(int);
-
+  //copy constructor
   Heap(const Heap&);
-
+  //assignment operator
   Heap& operator=(const Heap&);
-
+  //subscript operator
   T& operator[](int index) {
     return buffer[index];
   }
-
+  //destructor
   ~Heap();
-  
+  //shift the node up the tree
   void shift_up(int i);
-  
+  //shift the node down the tree
   void shift_down(int i);
-
+  //push the new item on the heap
   void push(T);
-
+  //remove and return the first item from the heap
   T pop();
-
+  //return the reference to the first item on the heap
   T& peek();
-
+  //returns the number of items on the heap
   int len() const;
-
+  //checks if there are items on the heap
   bool empty() const;
 };
 
@@ -63,32 +72,38 @@ Heap<T>::Heap(int prealloc_size) : size(prealloc_size), buffer(new T[prealloc_si
 
 
 template<typename T>
-Heap<T>::Heap(const Heap& other) : size(other.size), length(other.length), buffer(new T[other.size]) {
+Heap<T>::Heap(const Heap& other) : size(other.size), length(other.length) {
+  //if we are copying empty heap, theres no need to allocate memory just yet
   if(other.size == 0) {
-    buffer = nullptr;
+    this->buffer = nullptr;
     return;
   }
+  //allocate space for our items
+  this->buffer = new T[other.size];
+  //copy things from the other buffer to our buffer
   std::copy(&other.buffer[0], &other.buffer[other.length], &this->buffer[0]);
 }
 
 
 template<typename T>
 Heap<T>& Heap<T>::operator=(const Heap& other) {
+  //no need to copy ourselves
   if(this == &other)
     return *this;
-
+  //destroy the current buffer so we dont leak any memory
   if(this->buffer != nullptr)
     delete [] buffer;
-
+  
   this->size = other.size;
   this->length = other.length;
-  
+  //if the other buffer is empty theres no need to alloc memory
   if(this->size == 0) {
     this->buffer = nullptr;
     return *this;
   }
-
+  //alloc some space
   this->buffer = new T[other.size];
+  //copy buffer
   std::copy(&other.buffer[0], other.buffer[other.length], &this->buffer[0]);
   return *this;
 }
@@ -97,10 +112,11 @@ Heap<T>& Heap<T>::operator=(const Heap& other) {
 template<typename T>
 void Heap<T>::resize(int new_size) {
   size = new_size;
+  //alloc space for the new buffer
   T* new_buffer = new T[size];
-
+  //copy items from the other buffer to this buffer
   std::copy(&buffer[0], &buffer[length], &new_buffer[0]);
-  
+  //dealloc the old buffer so we dont leak memory
   delete [] buffer;
   buffer = new_buffer;
 }
@@ -120,20 +136,23 @@ void Heap<T>::expand_buffer() {
 
 template<typename T>
 Heap<T>::~Heap() {
+  //free memory
   delete [] buffer;
 }
 
 
 template<typename T>
 void Heap<T>::insert_to_buffer(T node) {
+  //if our buffer is empty we need to alloc some memory first
   if(buffer == nullptr) {
     buffer = new T[1];
     size = 1;
   }
-
+  //were about to overflow, we need to increase the buffer size
   if(length == size) {
     expand_buffer();
   }
+  //assign item at the end of the buffer
   buffer[length] = node;
   length++;
 }
@@ -141,7 +160,9 @@ void Heap<T>::insert_to_buffer(T node) {
 
 template<typename T>
 void Heap<T>::remove_from_buffer() {
+  //decreasing the length is enough, that memory will be overwritten by the next inserted node
   length--;
+  //we arent using that much space, lets free it
   if(length < size/2) {
     resize(size/2);
     return;
@@ -198,18 +219,22 @@ void Heap<T>::shift_down(int i) {
 
 template<typename T>
 void Heap<T>::push(T item) {
+  //insert the item at the end
   insert_to_buffer(item);
+  //and shift it to its correct place
   shift_up(length-1);
 }
 
 
 template<typename T>
 T Heap<T>::pop() {
+  //make a copy of our first object
   T item = buffer[0];
-
+  //overwrite the first object with the last one, we dont need it anymore
   buffer[0] = buffer[length-1];
+  //remove the old copy of the last object
   remove_from_buffer();
-
+  //and shift that object to its correct place
   shift_down(0);
   return item;
 }
--- proj2/src/data_structures/queue_heap/priority_queue_heap.hpp
+++ proj2/src/data_structures/queue_heap/priority_queue_heap.hpp
@@ -6,32 +6,45 @@
 template<typename T>
 struct PriorityQueueHeap {
 private:
+  //wow, a nested struct, these exist
  struct Node {
     T element;
     int priority;
-
+    //default constructor, so we can prealloc size within the heap
     Node() {}
+    //normal constructor
     Node(T element, int priotiry) : element(element), priority(priotiry) {}
+    //we overload comparison operators so the heap can operate on our nodes seemlessly 
     bool operator>(const Node& other) { return priority > other.priority; }
     bool operator<(const Node& other) { return priority < other.priority; }
   };
-
+  //heapy weapy UwU
   Heap<Node> heap;
 
 public:
+  //default constructor
   PriorityQueueHeap();
+  //constructor that preallocates some space
   PriorityQueueHeap(int);
+  //destructor
   ~PriorityQueueHeap();
-
+  
+  //insert item with priority
   void insert(T, int);
+  //return and remove the last item
   T extract_max();
 
+  //returns a reference to the last item
   T& peek();
 
+  //modify priority of the item at index
   void modify_key(int, int);
+  //remove item at index
   void remove(int);
 
+  //returns number of items in the priority queue
   int return_size() const;
+  //checks if the queue has items
   bool empty() const;
 };
 
@@ -67,10 +80,14 @@ T& PriorityQueueHeap<T>::peek() {
 
 template<typename T>
 void PriorityQueueHeap<T>::modify_key(int index, int new_priority) {
+  //we save the old priority for later
   int old_priority = heap[index].priority;
+  //we set the new priority
   heap[index].priority = new_priority;
+  //if the new priority is larger then we shift the item up
   if(new_priority > old_priority) {
     heap.shift_up(index);
+    //and if the new priority is lower the we shift the looser item down
   } else {
     heap.shift_down(index);
   }
@@ -79,8 +96,11 @@ void PriorityQueueHeap<T>::modify_key(int index, int new_priority) {
 
 template<typename T>
 void PriorityQueueHeap<T>::remove(int index) {
+  //set priority of our node to the highest
   heap[index].priority = heap[0].priority + 1;
+  //place it up top
   heap.shift_up(index);
+  //and then pop the first item, which is our node
   heap.pop();
 }
 
