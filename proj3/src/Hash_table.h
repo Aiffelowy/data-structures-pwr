7b0d244d7ef394b69d66b7bc5ee7ceea8e98e5e7
Adam <162824051+Ataru789@users.noreply.github.com>
2024-06-02 20:17:11 +0200

    fixed errors and added insert method

--- proj3/src/Hash_table.h
+++ proj3/src/Hash_table.h
@@ -1,57 +1,68 @@
-#ifndef hash_table.h
-#define hash_table.h
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 
 #include <iostream>
 
-//node class
+// Node class
 template <class T>
 class Node {
 public:
-	int key;
-	T value;
-	Node* next;
+    int key;
+    T value;
+    Node* next;
 
-	//constructor
-	Node(int key, T vaule) : key(key), value(value), next(nullptr);
-	
-	//destructor
-	~Node(void);
+    // Constructor
+    Node(int key, T value) : key(key), value(value), next(nullptr) {}
+
+    // Destructor
+    ~Node() {}
 };
 
-
-//Hash Table class
+// Hash Table class
 template <class T>
 class HashTable {
 private:
-	int size;
-	Node** table;
+    int size;
+    Node<T>** table;  
 
-	int hushFunction(int key) {
-		return key % size;
-	}
+    // Hash function
+    int hashFunction(int key) {
+        return key % size;
+    }
 
 public:
-	//construdtor
-	HashTable(int size) : size(size) {
-		table = new Node * [size]; // pointers array
-	}
+    // Constructor
+    HashTable(int size) : size(size) {
+        table = new Node<T>*[size]();  
+    }
 
-	//destructor
-	~HashTable() {
-		for (int i = 0; i < size; ++i) {
-			Node* current = table[i];
-			while (current != nullptr) {
-				Node* temp = current;
-				current = current->next;
-				delete temp;
-			}
-		}
-		delete[] table;
-	}
+    // Destructor
+    ~HashTable() {
+        for (int i = 0; i < size; ++i) {
+            Node<T>* current = table[i];
+            while (current != nullptr) {
+                Node<T>* temp = current;
+                current = current->next;
+                delete temp;
+            }
+        }
+        delete[] table;
+    }
 
-	void insert(int key, T value);
-	void remove(int key);
+    void insert(int key, T value);
+    void remove(int key);
 };
 
-#endif // !hash_table.h
+// Insert method
+template <class T>
+void HashTable<T>::insert(int key, T value) {
+    int index = hashFunction(key);
+    Node<T>* newNode = new Node<T>(key, value);
+
+    newNode->next = table[index];
+    table[index] = newNode;
+}
+
+
+#endif 
 
