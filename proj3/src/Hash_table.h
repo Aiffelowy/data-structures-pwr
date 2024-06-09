f9495cc406d825807eb6b1c3d4ccb4fff5788ff9
Adam <162824051+Ataru789@users.noreply.github.com>
2024-06-09 23:42:03 +0200

    back to things before rule of three
    i forgot to check if this is working, my baaad, im so sorry

--- proj3/src/Hash_table.h
+++ proj3/src/Hash_table.h
@@ -10,136 +10,108 @@ public:
     int key;
     T value;
     Node* next;
-
     // Constructor
     Node(int key, T value) : key(key), value(value), next(nullptr) {}
-
     // Destructor
     ~Node() {}
 };
 
-// hash Table class
+// Hash Table class
 template <class T>
 class HashTable {
 private:
     int size;
-    int capacity;
-    Node<T>** table; 
+    Node<T>** table;
 
-    // hash function
-    int hashFunction(int key) {
-        return key % size;
-    }
-    //helper to copy our list (for rule of three)
-    Node<T>* copyList(Node<T>* head) {
-        if (!head) return nullptr;
-        Node<T>* newHead = new Node<T>(head->key, head->value);
-        Node<T>* current = newHead;
-        Node<T>* source = head->next;
-        while (source) {
-            current->next = new Node<T>(source->key, source->value);
-            current = current->next;
-            source = source->next;
-        }
-        return newHead;
-    }
-
+    // Hash function
+    int hashFunction(int key);
 
 public:
-    // constructor
-    HashTable(int capacity) : size(0), capacity(capacity) {
-        table = new Node<T>*[capacity]();  
-    }
-
-    //copy constructor
-    HashTable(const HashTable& other) : size(other.size), capacity(other.capacity) {
-        table = new Node<T>*[capacity]();
-        for (int i = 0; i < capacity; ++i) {
-            table[i] = copyList(other.table[i]);
-        }
-    }
-    
-    //copy operator
-    HashTable& operator=(const HashTable& other) {
-        if (this == &other) {
-            return *this;
-        }
-
-        for (int i = 0; i < capacity; ++i) {
-            Node<T>* current = table[i];
-            while (current != nullptr) {
-                Node<T>* temp = current;
-                current = current->next;
-                delete temp;
-            }
-        }
-        delete[] table;
-
-        // Copy new resources
-        size = other.size;
-        capacity = other.capacity;
-        table = new Node<T>*[capacity]();
-        for (int i = 0; i < capacity; ++i) {
-            table[i] = copyList(other.table[i]);
-        }
-
-        return *this;
-    }
+    // Constructor
+    HashTable(int size);
 
     // Destructor
-    ~HashTable() {
-        for (int i = 0; i < capacity; ++i) {
-            Node<T>* current = table[i];
-            while (current != nullptr) {
-                Node<T>* temp = current;
-                current = current->next;
-                delete temp;
-            }
-        }
-        delete[] table;
-    }
+    ~HashTable();
 
     // Insert method
     void insert(int key, T value);
 
     // Remove method
     void remove(int key);
+
+    // Display method
+    void display();
 };
 
-// insert method
+template <class T>
+HashTable<T>::HashTable(int size) : size(size) {
+    table = new Node<T>*[size]();
+}
+
+template <class T>
+HashTable<T>::~HashTable() {
+    for (int i = 0; i < size; ++i) {
+        Node<T>* current = table[i];
+        while (current) {
+            Node<T>* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+    delete[] table;
+}
+
+template <class T>
+int HashTable<T>::hashFunction(int key) {
+    return key % size;
+}
+
 template <class T>
 void HashTable<T>::insert(int key, T value) {
     int index = hashFunction(key);
     Node<T>* newNode = new Node<T>(key, value);
-
-    newNode->next = table[index];
-    table[index] = newNode;
-    size++;
+    if (!table[index]) {
+        table[index] = newNode;
+    }
+    else {
+        Node<T>* current = table[index];
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
 }
 
-// remove method
 template <class T>
 void HashTable<T>::remove(int key) {
     int index = hashFunction(key);
     Node<T>* current = table[index];
     Node<T>* prev = nullptr;
-
-    while (current != nullptr) {
+    while (current) {
         if (current->key == key) {
-            if (prev == nullptr) {
-                table[index] = current->next;
-            }
-            else {
+            if (prev)
                 prev->next = current->next;
-            }
+            else
+                table[index] = current->next;
             delete current;
             return;
         }
         prev = current;
         current = current->next;
     }
-    size--;
 }
 
-#endif 
+template <class T>
+void HashTable<T>::display() {
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Bucket " << i << ": ";
+        Node<T>* current = table[i];
+        while (current) {
+            std::cout << "(" << current->key << ", " << current->value << ") ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+}
 
+#endif
