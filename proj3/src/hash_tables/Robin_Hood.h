9520a1019599a98109f032750c5e97e02851b09f
Adam <162824051+Ataru789@users.noreply.github.com>
2024-06-11 21:33:37 +0200

    added remove and display method

--- proj3/src/hash_tables/Robin_Hood.h
+++ proj3/src/hash_tables/Robin_Hood.h
@@ -1,6 +1,9 @@
 #ifndef ROBINHOOD_H
 #define ROBINGOOD_H
 
+#include <iostream>
+#include <utility>
+
 //entry class
 class Entry {
 public:
@@ -20,6 +23,7 @@ private:
 	Entry* table;
 	int size; 
 
+	//the reason why we do not use DJB2 HashFunction is to use power of Robin Hood hashing
 	int hashFunction(int key) {
 		return key % size;
 	}
@@ -35,17 +39,58 @@ public:
 	}
 
 	//insert method
-	void insert(int key, int value) {}
+	void insert(int key, int value) {
+		//calculate the index
+		int index = hashFunction(key);
+		int dist = 0;
+		while (table[index].isOccupied) {
+			//calculate the distance from original index if occupied
+			int current_dist = (index + size - hashFunction(table[index].key)) % size;
+			//if distance current key is shorter than new key, sawp place
+			if (current_dist < dist) {
+				std::swap(table[index].key, key);
+				std::swap(table[index].value, value);
+				dist = current_dist;
+			}
+			//moving until find empty spot
+			index = (index + 1) % size;
+			dist++;
+		}
+		table[index].key = key;
+		table[index].value = value;
+		table[index].isOccupied = true;
+	}
 
 	//remove method
-	void remove(int key) {}
+	void remove(int key) {
+		int index = hashFunction(key);
+		int dist = 0;
+		while (table[index].isOccupied) {
+			if (table[index].key == key) {
+				table[index].isOccupied = false;
+				table[index].key = 0;
+				table[index].value = 0;
+				return;
+			}
+			index = (index + 1) % size;
+			dist++;
+		}
+	}
 
 	//display method
-	void display() {}
+	void display() {
+		for (int i = 0; i < size; ++i) {
+			if (table[i].isOccupied) {
+				std::cout << i << ": " << table[i].key << " => " << table[i].value << std::endl;
+			}
+			else {
+				std::cout << i << ": " << "EMPTY" << std::endl;
+			}
+		}
+	}
 
-	//rehash method
-	void rehash(int start_index) {}
 };
+
 
 #endif 
 
