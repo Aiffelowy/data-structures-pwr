55881de61782c8efaf178e759c94b2664131b736
Adam <162824051+Ataru789@users.noreply.github.com>
2024-06-14 14:00:08 +0200

    a little fix

--- proj3/src/hash_tables/Robin_Hood.h
+++ proj3/src/hash_tables/Robin_Hood.h
@@ -1,96 +1,117 @@
 #ifndef ROBINHOOD_H
-#define ROBINGOOD_H
+#define ROBINHOOD_H
 
 #include <iostream>
 #include <utility>
 
-//entry class
+// Entry class
 class Entry {
 public:
-	int key;
-	int value;
-	bool isOccupied;
+    int key;
+    int value;
+    bool isOccupied;
 
-	//constructor
-	Entry() : key(0), value(0), isOccupied(false) {}
+    // Constructor
+    Entry() : key(0), value(0), isOccupied(false) {}
 
-	//destructor
-	~Entry() {}
+    // Destructor
+    ~Entry() {}
 };
 
 class RobinHood {
 private:
-	Entry* table;
-	int size; 
+    Entry* table;
+    int size;
+    int numElements;
 
-	//the reason why we do not use DJB2 HashFunction is to use power of Robin Hood hashing
-	int hashFunction(int key) {
-		return key % size;
-	}
+    // Hash function
+    int hashFunction(int key) {
+        return key % size;
+    }
+
+    // Rehash function
+    void rehash() {
+        int oldSize = size;
+        size *= 2;
+        Entry* oldTable = table;
+        table = new Entry[size];
+        numElements = 0;
+
+        for (int i = 0; i < oldSize; ++i) {
+            if (oldTable[i].isOccupied) {
+                insert(oldTable[i].key, oldTable[i].value);
+            }
+        }
+
+        delete[] oldTable;
+    }
+
 public:
-	//constructor
-	RobinHood(int table_size) : size(table_size) {
-		table = new Entry[size];
-	}
+    // Constructor
+    RobinHood(int table_size) : size(table_size), numElements(0) {
+        table = new Entry[size];
+    }
 
-	//destructor
-	~RobinHood() {
-		delete[] table;
-	}
+    // Destructor
+    ~RobinHood() {
+        delete[] table;
+    }
 
-	//insert method
-	void insert(int key, int value) {
-		//calculate the index
-		int index = hashFunction(key);
-		int dist = 0;
-		while (table[index].isOccupied) {
-			//calculate the distance from original index if occupied
-			int current_dist = (index + size - hashFunction(table[index].key)) % size;
-			//if distance current key is shorter than new key, sawp place
-			if (current_dist < dist) {
-				std::swap(table[index].key, key);
-				std::swap(table[index].value, value);
-				dist = current_dist;
-			}
-			//moving until find empty spot
-			index = (index + 1) % size;
-			dist++;
-		}
-		table[index].key = key;
-		table[index].value = value;
-		table[index].isOccupied = true;
-	}
+    // Insert method
+    void insert(int key, int value) {
+        if (numElements >= size / 2) {
+            rehash();
+        }
 
-	//remove method
-	void remove(int key) {
-		int index = hashFunction(key);
-		int dist = 0;
-		while (table[index].isOccupied) {
-			if (table[index].key == key) {
-				table[index].isOccupied = false;
-				table[index].key = 0;
-				table[index].value = 0;
-				return;
-			}
-			index = (index + 1) % size;
-			dist++;
-		}
-	}
+        int index = hashFunction(key);
+        int dist = 0;
 
-	//display method
-	void display() {
-		for (int i = 0; i < size; ++i) {
-			if (table[i].isOccupied) {
-				std::cout << i << ": " << table[i].key << " => " << table[i].value << std::endl;
-			}
-			else {
-				std::cout << i << ": " << "EMPTY" << std::endl;
-			}
-		}
-	}
+        while (table[index].isOccupied) {
+            int current_dist = (index + size - hashFunction(table[index].key)) % size;
+            if (current_dist < dist) {
+                std::swap(table[index].key, key);
+                std::swap(table[index].value, value);
+                dist = current_dist;
+            }
+            index = (index + 1) % size;
+            dist++;
+        }
 
+        table[index].key = key;
+        table[index].value = value;
+        table[index].isOccupied = true;
+        numElements++;
+    }
+
+    // Remove method
+    void remove(int key) {
+        int index = hashFunction(key);
+        int dist = 0;
+
+        while (table[index].isOccupied) {
+            if (table[index].key == key) {
+                table[index].isOccupied = false;
+                table[index].key = 0;
+                table[index].value = 0;
+                numElements--;
+                return;
+            }
+            index = (index + 1) % size;
+            dist++;
+        }
+    }
+
+    // Display method
+    void display() {
+        for (int i = 0; i < size; ++i) {
+            if (table[i].isOccupied) {
+                std::cout << i << ": " << table[i].key << " => " << table[i].value << std::endl;
+            }
+            else {
+                std::cout << i << ": " << "EMPTY" << std::endl;
+            }
+        }
+    }
 };
 
-
-#endif 
-
+#endif
